02fbea6cf103b9facea8295bfcc958db8da286b8
Martin Ducau <martin@ducau.ar>
2022-12-29 19:43:58 -0300

    fix roulette transition speed

--- JS/roulette.js
+++ JS/roulette.js
@@ -1,25 +1,25 @@
 import getData from "./getData.js";
 
-window.addEventListener( "DOMContentLoaded", async () => {
+window.addEventListener("DOMContentLoaded", async () => {
   const products = await getData();
-  const container = document.getElementById( "roulette" );
+  const container = document.getElementById("roulette");
   let slide = 0;
 
-  products.forEach( ( product ) => {
+  products.forEach((product) => {
     const slideId = slide++;
-    const div = document.createElement( "div" );
-    div.classList.add( "carousel__container" );
+    const div = document.createElement("div");
+    div.classList.add("carousel__container");
     div.innerHTML = `<img class='carousel__container__alfajor' src=${product.img} alt=${product.name}>
                       <button class='btn__hidden btn__flavor slider__opener' id=${slideId}>${product.name}</button>
                    `;
-    container.appendChild( div );
-  } );
+    container.appendChild(div);
+  });
 
-  $( document ).ready( function () {
-    let total = $( ".carousel__container" ).length,
-      rand = Math.floor( Math.random() * total );
+  $(document).ready(function () {
+    let total = $(".carousel__container").length,
+      rand = Math.floor(Math.random() * total);
 
-    $( ".container__main__roulette__placeholder" ).slick( {
+    $(".container__main__roulette__placeholder").slick({
       initialSlide: rand,
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -28,35 +28,38 @@ window.addEventListener( "DOMContentLoaded", async () => {
       speed: 3000,
       variableWidth: true,
       adaptiveHeight: false,
-      cssEase: 'linear' ,
+      cssEase: "linear",
       centerMode: true,
       arrows: false,
       dragabble: false,
       infinite: true,
       pauseOnFocus: false,
-      pauseOnHover: false
-
-    } );
-  } 
-  );
-  $( "#roulette_btn" ).click( function () {
-    $( ".container__main__roulette__placeholder" ).slick(
+      pauseOnHover: false,
+    });
+  });
+  $("#roulette_btn").click(function () {
+    $(".container__main__roulette__placeholder").slick(
       "slickSetOption",
       "speed",
-      300,
+      400,
       true
-    )
-    $(".slick-.container__main__roulette__placeholder .slick-active").css("transition", "all 0.2s ease-in-out")
+    );
+    $( ".slick-slide").css(
+      "transition",
+      "all 0.399s ease-in-out"
+    );
 
-    setTimeout( function () {
-      $( ".container__main__roulette__placeholder" ).slick( "slickPause" );
-      $( ".slick-current button" ).removeClass( "btn__hidden" );
-      $( ".container__main__roulette__placeholder" ).addClass( "fireworks" );
-    }, 5000 );
-    $( this ).html( "Girar Nuevamente" );
-    $( this ).removeClass( "btn" );
-    $( this ).addClass( "link" );
-    $( this ).click( function () { $( ".slick-current button" ).addClass( "btn__hidden" ), $( ".container__main__roulette__placeholder" ).removeClass( "fireworks" ) } )
-  } );
-  
-} );
+    setTimeout(function () {
+      $(".container__main__roulette__placeholder").slick("slickPause");
+      $(".slick-current button").removeClass("btn__hidden");
+      $(".container__main__roulette__placeholder").addClass("fireworks");
+    }, 5000);
+    $(this).html("Girar Nuevamente");
+    $(this).removeClass("btn");
+    $(this).addClass("link");
+    $(this).click(function () {
+      $(".slick-current button").addClass("btn__hidden"),
+        $(".container__main__roulette__placeholder").removeClass("fireworks");
+    });
+  });
+});
