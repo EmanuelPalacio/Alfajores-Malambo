c2e59e3162ebc5564e71fcb19c108dfa2453b5f2
ducau84 <martin@ducau.ar>
2023-01-10 12:00:40 -0300

    fix routing extensions

--- JS/getData.js
+++ JS/getData.js
@@ -1,6 +1,6 @@
 const getData = async () => {
   try {
-    const response = await fetch( "https://emanuelpalacio.github.io/Alfajores-Malambo/data/products.json" );
+    const response = await fetch( "../data/products.json" );
     const data = await response.json();
     return data;
   } catch ( error ) {
